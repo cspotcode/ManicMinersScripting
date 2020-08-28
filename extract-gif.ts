7eb31e8ad58d5da322dfd4ff852d9b154a7207ac
Andrew Bradley <cspotcode@gmail.com>
2020-08-28 18:09:43 -0400

    updates

--- extract-gif.ts
+++ extract-gif.ts
@@ -6,7 +6,7 @@ import streamToArray from 'stream-to-array';
 import { outdent } from 'outdent';
 import fs from 'fs';
 
-const enum TileIds {
+const enum TileId {
     Ground = 1,
     RubbleL1 = 2,
     Lava = 6,
@@ -19,6 +19,18 @@ const enum TileIds {
     PowerPath1Powered = 17,
     Off = PowerPathBuilding,
     On = PowerPathBuildingPowered
+}
+
+const enum ArrowColor {
+    red = 'red',
+    green = 'green',
+    blue = 'blue',
+    darkgreen = 'darkgreen',
+    yellow = 'yellow',
+    white = 'white',
+    black = 'black',
+    On = white,
+    Off = black
 }
 
 async function main() {
@@ -38,16 +50,17 @@ async function main() {
         delay: 0.1,
         firstFrameDelay: 0.1,
         startTime: 1,
-        loop: true
+        loop: true,
+        renderMethod: 'highlights'
     });
 
     console.log(frames[0].width, frames[0].height);
-    patchMap('../Levels/gif-to-mm.dat', script);
+    patchMap('./gif-to-mm.dat', script);
 }
 
 function patchMap(mapPath: string, script: string) {
     const map = fs.readFileSync(mapPath, 'utf8');
-    let patchedMap = map.replace(/(script *\{)[\s\S\r\n]+/, ($0, $1) => outdent `
+    let patchedMap = map.replace(/(script *\{)[\s\S\r\n]+/, ($0, $1) => outdent`
         ${ $1 }
         ${ script }
         }
@@ -72,7 +85,7 @@ async function parseFrames(path: string = 'image.gif') {
         const dataBuffer = Buffer.concat(array);
         const png = new PNG(dataBuffer);
         const buffer: Buffer = await new Promise(res => png.decodePixels(res));
-        const {width, height} = png;
+        const { width, height } = png;
         return {
             getPixel(x: number, y: number) {
                 if(x > width) return [0, 0, 0];
@@ -110,6 +123,7 @@ interface ScriptOptions {
     firstFrameDelay: number;
     startTime: number;
     loop: boolean;
+    renderMethod: 'tiles' | 'highlights';
 }
 function framesToScript(options: ScriptOptions) {
     const {
@@ -118,37 +132,79 @@ function framesToScript(options: ScriptOptions) {
         width, height,
         mapXOffset, mapYOffset,
         xOffset, yOffset,
-        xSampleRate, ySampleRate
+        xSampleRate, ySampleRate,
+        renderMethod
     } = options;
     let script = '';
+
+    if(renderMethod === 'highlights') {
+        eachPixel((x, y) => {
+            script += outdent`
+                arrow ${arrowName(x, y, 'off', 'a') }=${ ArrowColor.Off }
+                arrow ${arrowName(x, y, 'off', 'b') }=${ ArrowColor.Off }
+                arrow ${arrowName(x, y, 'on', 'a') }=${ ArrowColor.On }
+                arrow ${arrowName(x, y, 'on', 'b') }=${ ArrowColor.On }
+
+            `;
+        });
+        script += 'init::;'
+        eachPixel((x, y) => {
+            script += outdent`
+                    highlight:${mapCoords(x, y) },${ arrowName(x, y, 'off', 'a') };
+                    highlight:${mapCoords(x, y) },${ arrowName(x, y, 'on', 'a') };
+
+            `;
+        });
+        script += outdent`
+            truewait:0.5;
+
+        `;
+        eachPixel((x, y) => {
+            script += `
+                    highlight:${mapCoords(0, 0) },${ arrowName(x, y, 'off', 'b') };
+                    highlight:${mapCoords(0, 0) },${ arrowName(x, y, 'on', 'b') };
+
+            `;
+        });
+    }
+
     script += outdent`
-            when(time:${startTime})[animation]
-            animation::
+            when(time:${startTime })[animation]
+            animation::;
+
         `;
 
     let previousFrame = undefined;
-    for (const frame of frames) {
-        for (let y = 0; y < height; y++) {
-            for (let x = 0; x < width; x++) {
-                const xPixel = xOffset + x * xSampleRate;
-                const yPixel = yOffset + y * ySampleRate;
-                // const [r, g, b] = frame.getPixel(xOffset + x * xSampleRate, yOffset + y * ySampleRate);
-                const r = frame.getRed(xPixel, yPixel);
-                const on = r > 50;
-                if (previousFrame) {
-                    // const [pr, pg, pb] = previousFrame.getPixel(xOffset + x, yOffset + y);
-                    const pr = previousFrame.getRed(xPixel, yPixel);
-                    const pon = pr > 50;
-                    if (pon === on) continue;
-                }
+    for(const frame of frames) {
+        eachPixel((x, y) => {
+            const xPixel = xOffset + x * xSampleRate;
+            const yPixel = yOffset + y * ySampleRate;
+            // const [r, g, b] = frame.getPixel(xOffset + x * xSampleRate, yOffset + y * ySampleRate);
+            const r = frame.getRed(xPixel, yPixel);
+            const on = r > 50;
+            if(previousFrame) {
+                // const [pr, pg, pb] = previousFrame.getPixel(xOffset + x, yOffset + y);
+                const pr = previousFrame.getRed(xPixel, yPixel);
+                const pon = pr > 50;
+                if(pon === on) return;
+            }
+            if(renderMethod === 'tiles') {
                 script += outdent`
-                        place:${mapYOffset + y},${mapXOffset + x},${ on ? TileIds.On : TileIds.Off };
+                            place:${mapCoords(x, y) },${ on ? TileId.On : TileId.Off };
+
+                        `;
+            } else {
+                script += outdent`
+                        highlight:${mapCoords(x, y) },${ arrowName(x, y, on ? 'on' : 'off', 'a') };
+                        highlight:${mapCoords(x, y) },${ arrowName(x, y, on ? 'on' : 'off', 'b') };
+                        highlight:${mapCoords(0, 0) },${ arrowName(x, y, on ? 'off' : 'on', 'a') };
+                        highlight:${mapCoords(0, 0) },${ arrowName(x, y, on ? 'off' : 'on', 'b') };
 
                     `;
             }
-        }
+        });
         script += outdent`
-            wait:${previousFrame ? delay : firstFrameDelay};
+            wait:${previousFrame ? delay : firstFrameDelay };
 
         `;
         previousFrame = frame;
@@ -157,10 +213,25 @@ function framesToScript(options: ScriptOptions) {
         script += 'animation;\n';
     }
     return script;
+
+    function eachPixel(cb: (x: number, y: number) => void) {
+        for(let y = 0; y < height; y++) {
+            for(let x = 0; x < width; x++) {
+                cb(x, y);
+            }
+        }
+    }
+    function mapCoords(x: number, y: number) {
+        return `${ mapYOffset + y },${ mapXOffset + x }`;
+    }
+}
+
+function arrowName(x: number, y: number, onOff: 'on' | 'off', variant: 'a' | 'b') {
+    return `render_${ x }_${ y }_${ onOff }_${ variant }`;
 }
 
 function* range(count) {
-    for (let i = 0; i < count; i++) yield i;
+    for(let i = 0; i < count; i++) yield i;
 }
 
 main();
