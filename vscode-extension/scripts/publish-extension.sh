f7963e27ed6c2b473cb3fc6bceae1e839819a51e
Andrew Bradley <cspotcode@gmail.com>
2020-09-24 13:33:41 -0400

    fix

--- vscode-extension/scripts/publish-extension.sh
+++ vscode-extension/scripts/publish-extension.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 set -euxo pipefail
-shopt -s inherit_errexit
+#shopt -s inherit_errexit
 
 # This requires setting up creds to push back to github
-# npm version patch
-# git push && git push --tags
+npm version patch
+git push && git push --tags
 
-# This is the lazy approach
-npm version "0.0.1-$(node -p "(new Date).toISOString().replace(/:/g, '-')")"
+# This is the lazy approach (but did not work because vsce does not support this kind of version number)
+# npm version "0.0.1-$(node -p "(new Date).toISOString().replace(/:/g, '-')")"
 
 vsce package
 vsce publish
