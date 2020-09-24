bafb1c99850ef9f3aa9f84abf617a2ff6ab2d857
Andrew Bradley <cspotcode@gmail.com>
2020-09-24 13:01:09 -0400

    ifx

--- vscode-extension/scripts/publish-extension.sh
+++ vscode-extension/scripts/publish-extension.sh
@@ -9,5 +9,5 @@ shopt -s inherit_errexit
 # This is the lazy approach
 npm version "0.0.1-$(node -p "(new Date).toISOString().replace(/:/g, '-')")"
 
-vcse package
-vcse publish
+vsce package
+vsce publish
