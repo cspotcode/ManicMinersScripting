459b5320ea88a4cd6733b2763432b306ada9a84d
Andrew Bradley <cspotcode@gmail.com>
2020-09-24 13:42:04 -0400

    fix

--- vscode-extension/scripts/publish-extension.sh
+++ vscode-extension/scripts/publish-extension.sh
@@ -2,12 +2,13 @@
 set -euxo pipefail
 #shopt -s inherit_errexit
 
-git branch -f master
-git checkout master
-
 # This requires setting up creds to push back to github
 npm version patch
-git push --set-upstream origin master && git push --tags
+git add package.json
+version="$(node -p "require('./package').version")"
+git commit -m "vscode-extension v$version"
+git tag "vscode-extension-v$version"
+git push && git push --tags
 
 # This is the lazy approach (but did not work because vsce does not support this kind of version number)
 # npm version "0.0.1-$(node -p "(new Date).toISOString().replace(/:/g, '-')")"
